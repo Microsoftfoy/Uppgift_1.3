2cca1589b2199b6820e74085157b307f8340da9f
ercr07001 <ercr07001@E-PW05CGR0>
2024-09-11 12:47:30 +0200

    Add project files.

---
+++ Uppgift_1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   / \\");
+            Console.WriteLine("  /   \\");
+            Console.WriteLine(" / \"\"  \\");
+            Console.WriteLine("/_______\\");
+        }
+    }
+}
